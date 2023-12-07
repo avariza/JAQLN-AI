db46019c068b47cfce9da0f6b75418bf3f8bafdc
angieariza <angie.variza@gmail.com>
2023-12-07 00:05:49 -0500

    tokenalization clean

--- tasks/model/nlp_utilities.py
+++ tasks/model/nlp_utilities.py
@@ -1,4 +1,8 @@
 import regex
+import spacy
+nlp = spacy.load("en_core_web_sm")
+
+
 
 def clean_text(series, keep_punctuation=".,;?!"):
     """
@@ -16,3 +20,28 @@ def clean_text(series, keep_punctuation=".,;?!"):
     cleaned_series = series.str.replace(pattern, '', regex=True)
     
     return cleaned_series
+
+def tokenize_text(text):
+    """
+    The `tokenize_text` function takes in a text as input and returns a list of tokens, where each token
+    represents a word or punctuation mark in the text.
+    
+    :param text: The `text` parameter is a string that represents the text you want to tokenize
+    :return: The function `tokenize_text` returns a list of tokens, where each token is a string.
+    """
+    doc = nlp(text)
+    tokens = [token.text for token in doc]
+    return tokens
+
+def tokenize_text_with_numbers(text):
+    """
+    The function `tokenize_text_with_numbers` takes in a text as input, tokenizes it using the `nlp`
+    function, and returns a list of tokens excluding any digits.
+    
+    :param text: The `text` parameter is a string that represents the text you want to tokenize
+    :return: The function `tokenize_text_with_numbers` returns a list of tokens (words) from the input
+    text, excluding any tokens that are digits (numbers).
+    """
+    doc = nlp(text)
+    tokens = [token.text for token in doc if not token.is_digit]
+    return tokens
